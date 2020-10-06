49d3f87a955accaefdeb093939c5bf29fcdcdd39
agusmazzeo <amazzeo@moorea.io>
2020-10-06 02:02:19 -0300

    feat: Updated app.py

--- application/src/nubi_test/app.py
+++ application/src/nubi_test/app.py
@@ -1,7 +1,9 @@
 import os
 from flask import Flask
 
-from nubi_test.init import config, register,logger
+from nubi_test.init import config, register, logger, mongo
+
+
 
 def start_server():
     app = create_app()
@@ -11,14 +13,14 @@ def start_server():
 def init_app(app):
     app.config = config.load_config(app)
     app.logger = logger.configure_logger('app')
-    MongoConn(app)  
+    mongo.MongoConn(app)
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
-    
+
     with app.app_context():
-        
+
         init_app(app)
         register.register_blueprints(app)
 
